fac8aaf0ca27876d594e14069eab2301c33e445e
abdel <zbio234@gmail.com>
2024-09-10 00:00:42 +0200

    1

--- components/r3fUtils.jsx
+++ components/r3fUtils.jsx
@@ -4,11 +4,11 @@ export function generateBlockData(level)
     {
         const { nodes, materials,animations } = useGLTF('/model_2.glb');
         
-        if(level >= 1 && level < 6)
+        if(level >= 1 && level < 4)
         {
             return [nodes.block_1.geometry,nodes.block_1_left.geometry,nodes.block_1_right.geometry,'block1laction','block1raction','block_1_left','block_1_right','bricktxt1.jpg']
         }
-        else if(level >= 6 && level < 10)
+        else if(level >= 4 && level < 10)
         {
             return [nodes.block_2.geometry,nodes.block_2_left.geometry,nodes.block_2_right.geometry,'block_2_leftAction','block_2_rightAction','block_2_left','block_2_right','block_1_txt.jpg']
         }
